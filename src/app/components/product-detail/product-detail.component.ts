9ebf32dccf7edf3c415189cca1b84b77d7612ccf
JevticJovana <jevticjovana99@gmail.com>
2021-11-28 16:06:43 +0100

    Obrisani komentari - Product Detail page

--- src/app/components/product-detail/product-detail.component.ts
+++ src/app/components/product-detail/product-detail.component.ts
@@ -1,7 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Products } from 'src/app/classes/Products';
-import { AllProductsComponent  } from 'src/app/pages/all-products/all-products.component';
 import { apiService } from 'src/app/services/api.service';
 import { LoaderService } from 'src/app/loader/loader.service';
 
@@ -13,7 +12,6 @@ import { LoaderService } from 'src/app/loader/loader.service';
 export class ProductDetailComponent implements OnInit {
 
   @Input() product: any;
-  //product: Products | undefined;
   pageTitle = 'Informacije o proizvodu: ';
   productId: string = '';
 
@@ -25,26 +23,13 @@ export class ProductDetailComponent implements OnInit {
   ngOnInit(): void {
     let id = String(this.route.snapshot.paramMap.get('id'));
     this.productId = id;
-    // console.log(id);
-    // if(id != null)
-    //   this.getProductApi(id);
     this.getProductApi(this.productId);
   }
-
-  // getProductId():void {
-  //   let id = this.route.snapshot.paramMap.get('id');
-  //   this.productId = id;
-  //   console.log(id);
-  // }
 
   getProductApi(id: string): void {
     this.apiService.getProduct(id).subscribe(      
       (data: any) => {
         this.product = data.product[0];
-        // console.log(this.product);
-        // console.log(this.product.name);
-        // console.log(data.name);
-        // console.log(data.product[0].name);
     });
   }
 
