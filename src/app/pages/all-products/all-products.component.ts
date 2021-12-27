39a389631f00e7affc997acbd033094aaac73598
uroskg99 <stanojkovu@gmail.com>
2021-12-27 18:11:15 +0100

    Zavrseno sredjivanje all products stranice

--- src/app/pages/all-products/all-products.component.ts
+++ src/app/pages/all-products/all-products.component.ts
@@ -19,6 +19,17 @@ export class AllProductsComponent implements OnInit {
   productsList: Products[] = [];
   totalProducts: number = 0;
   lastPage: number = 0;
+  selectedCategories: any[] = [];
+  showFilters: boolean = false;
+
+  categories = [
+    {name: "Čokoladni proizvodi", checked: false, n:0},
+    {name: "Piće, kafa i čaj", checked: false, n:1},
+    {name: "Sve za kuću", checked: false, n:2},
+    {name: "Kućni ljubimci", checked: false, n:3},
+    {name: "Smrznuti proizvodi", checked: false, n:4},
+    {name: "Lična higijena i kozmetika", checked: false, n:5},
+  ];
 
   getProductsApi(category: string, page: number) {
     this._apiService.getProducts(category, page).subscribe((data: any) => {
@@ -30,6 +41,26 @@ export class AllProductsComponent implements OnInit {
 
   ngOnInit() {
     this.getProductsApi(this.category, this.page);
+  }
+
+  getSelectedCategories() {
+    const selectedCategories = this.categories.filter(cat => cat.checked).map(cat => cat.name);
+    return selectedCategories;
+  }
+
+  checkedCategory(e: any, category: any) {
+    this.categories[category.n].checked = !this.categories[category.n].checked;
+    this.selectedCategories = this.getSelectedCategories();
+    this.category = '';
+    for (let index = 0; index < this.selectedCategories.length; index++) {
+      this.category += this.selectedCategories[index] + ':';
+    }
+    this.category = this.category.slice(0, -1);
+    this.getProductsApi(this.category, this.page);
+  }
+
+  toggleFilters() {
+    this.showFilters = !this.showFilters;
   }
 
   previousPage() {
